0dc3a12bebb3de5c98e29c3976a0cb605557d86b
Franklin Liou <franklin@mvdatacenter.com>
2024-03-29 11:28:51 -0700

    add infer_trade.py

--- infer_trade.py
+++ infer_trade.py
@@ -12,13 +12,13 @@ def main():
     stop = 6000
     for i in range(start, stop, 100):
         print("Pulling latest data from exchange...")
-        test_begin_day = 6500 + i
-        test_end_day = 7000 + i
+        test_begin_day = 4500 + i
+        test_end_day = 5000 + i
         stock_test = [asset[test_begin_day:test_end_day] for asset in asset_list]
         record, total_gain, total_cost = stat_arb_success(stock_test, model)
         if (total_gain > total_cost):
             print(f"Trade executed for a profit of {total_gain - total_cost} Arbitrage is successful!")
-        print(stat_arb_success_buy_and_hold_only_once(stock_test))
+        stat_arb_success_buy_and_hold_only_once(stock_test)
 
 if __name__ == '__main__':
     main()
